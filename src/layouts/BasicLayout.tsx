8dd7adc37f1dc47834cf6e7bc502b6fb3d5ef0cc
LaughingZhu <474268433@qq.com>
2020-08-23 00:00:15 +0800

    layout 布局

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import BG from '../assets/js/bg.js'
 import "./index.less";
+import Header from '../component/header';
+import Aside from '../component/aslide'
+import SlideBar from '../component/slidebar'
 
 interface IProps {
   children: any,
@@ -22,8 +24,16 @@ class BasicLayout extends Component <IProps, IState> {
     return (
 
       <div className="layout" >
-        {children}
-        
+        <div className="layout-header">
+          <div className="layout-header-container"><Header /></div>
+        </div>
+        <div className="layout-container">
+          <div className="layout-container-aside"><Aside /></div>
+          <div className="layout-container-content flex">
+            <div className="layout-container-content-main">{children}</div>
+            <div className="layout-container-content-slidebar"><SlideBar /></div>
+          </div>
+        </div>
       </div>
     );
 
