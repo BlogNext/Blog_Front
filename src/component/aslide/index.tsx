8384fadd67425e99a16f1531e666c93e43fa94c6
LaughingZhu <474268433@qq.com>
2020-08-23 16:48:13 +0800

    项目布局设置

--- src/component/aslide/index.tsx
+++ src/component/aslide/index.tsx
@@ -8,8 +8,13 @@ export default function Aside (props: any) {
       <div className="component-aside_info flex">
         <div className="component-aside_info-avatar"/>
         <div className="component-aside_info-name">LaughingZhu</div>
+        <div className="component-aside_info-slogan">Make or miss win or lose I put my name on it</div>
       </div>
-      <Divider style={{ backgroundColor: '#2e3344', height: '1px'}} />
+      <Divider style={{ backgroundColor: '#2e3344', height: '1px', marginTop: '0'}} />
+
+      <div className="component-menu">
+        ddd
+      </div>
     </div>
   )
 }
--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import {HomeOutlined} from '@ant-design/icons';
+import { Input } from 'antd'
 import './style.less'
+
+const { Search } = Input;
 
 export default function Header (props: any) {
   return(
-    <div className="component-header">
-      <div className="component-header_info">
-        info
+    <div className="component-header flex">
+      <div className="component-header_title flex">
+        {/* <HomeOutlined className='component-header_title_icon' /> */}
+        <span>LaughingZhu's Blog</span>
+      </div>
+
+      <div className="component-header_container flex">
+        <Search
+          className='component-header_container_search'
+          placeholder="search project"
+          onSearch={value => console.log(value)}
+          style={{ width: 200 }}
+        />
+      </div>
+
+      <div className="component-header_tools">
+
       </div>
     </div>
   )
