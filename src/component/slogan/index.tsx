e59f921e5b3bc1bf5e2f45c680e493686f0ba2d4
LaughingZhu <474268433@qq.com>
2020-09-15 16:32:49 +0800

    title  add  spotlight  聚光灯效果

--- src/component/slogan/index.tsx
+++ src/component/slogan/index.tsx
@@ -11,8 +11,8 @@ export default function Slogan (props: any) {
 
   return (
     <div className="component-slogan flex">
-      <div className="component-slogan-name">LaughingZhu's Blog</div>
-      <div className="component-slogan-desc">桌上的文字太拥挤 想出去走走去散心</div>
+      <div className="component-slogan-name"  data-spotlight={`LaughingZhu's Blog`}>LaughingZhu's Blog</div>
+      <div className="component-slogan-desc" >桌上的文字太拥挤 想出去走走去散心</div>
     </div>
   )
 }
