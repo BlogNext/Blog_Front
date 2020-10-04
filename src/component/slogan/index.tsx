30b5ec2c459c574171e72ece4ab207e0f4f909a6
LaughingZhu <474268433@qq.com>
2020-10-04 17:50:03 +0800

    百度统计测试

--- src/component/slogan/index.tsx
+++ src/component/slogan/index.tsx
@@ -8,10 +8,15 @@ export default function Slogan (props: any) {
     console.log('slogan-render')
   }, [''])
 
+  const sloganClick = () => {
+    console.log('slogan click');
+    (window as any)._hmt.push(['_trackEvent', 'slogan', 'click']);
+  }
+
 
   return (
     <div className="component-slogan flex">
-      <div className="component-slogan-name"  data-spotlight={`LaughingZhu's Blog`}>LaughingZhu's Blog</div>
+      <div className="component-slogan-name" onClick={() => sloganClick()}  data-spotlight={`LaughingZhu's Blog`}>LaughingZhu's Blog</div>
       <div className="component-slogan-desc" >桌上的文字太拥挤 想出去走走去散心</div>
     </div>
   )
