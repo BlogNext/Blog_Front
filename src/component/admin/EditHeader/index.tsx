d34ff5af2200272bb5d765d3402578f98c119708
LaughingZhu <474268433@qq.com>
2020-10-19 17:58:38 +0800

    编辑文章头部导航

--- src/component/admin/EditHeader/index.tsx
+++ src/component/admin/EditHeader/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-
+import { BarsOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import './style.less'
+import { Button } from 'antd';
 
 function EditHeader (props: any) {
 
@@ -10,8 +11,21 @@ function EditHeader (props: any) {
   const [ author, setAuthor ] = useState('')
 
   return (
-    <div className="admin-component-editHeader">
-      header
+    <div className="admin-component-editHeader flex">
+      <div className="admin-component-editHeader-left flex">
+        <BarsOutlined className="admin-component-editHeader-left-menu" />
+
+        <div className="admin-component-editHeader-left-tag">请选择标签</div> /
+        <div className="admin-component-editHeader-left-title">无标题</div>
+        <div className="admin-component-editHeader-left-info">
+          <ExclamationCircleOutlined />
+        </div>
+        <div className="admin-component-editHeader-left-record">最后更改于今天{new Date().toLocaleTimeString()}</div>
+      </div>
+        
+      <div className="admin-component-editHeader-right">
+        <Button onClick={props.onSubmit} className="admin-component-editHeader-right-submit" type="primary">发布</Button>
+      </div>
     </div>
   )
 }
