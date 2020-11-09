a7fae7c881cd18f7b64437df3079ef908b92c773
LaughingZhu <474268433@qq.com>
2020-11-09 09:03:57 +0800

    删除header oauthcode 字段

--- src/utils/request.js
+++ src/utils/request.js
@@ -48,15 +48,15 @@ const request = extend({
 request.interceptors.request.use((url, options) => {
   Toast.loading('加载中', 0)
 
-  const access_token = localStorage.getItem('blog_token')
+  // const access_token = localStorage.getItem('blog_token')
 
-  options.headers = {
-    // 'oauthCode': access_token
-  }
+  // options.headers = {
+  //   // 'oauthCode': access_token
+  // }
   return (
     {
       url,
-      options: { ...options },
+      // options: { ...options },
     }
   );
 });
