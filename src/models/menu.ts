50cc9a163a42bab8b8cef0750f0a4bd4fec79bbf
LaughingZhu <474268433@qq.com>
2020-12-04 15:15:52 +0800

    新增 基于reducer 的dva管理菜单和列表信息

---
+++ src/models/menu.ts
@@ -0,0 +1,116 @@
+import { Reducer } from 'redux';
+import { Effect } from 'dva';
+
+import { getList } from '../../src/api/api'
+
+
+export interface StateType {
+  menuList: any,
+  page: number,
+  pageSize: number
+  total: number,
+  typeList: any,
+  type_id: number
+}
+
+export interface LoginModelType {
+  namespace: string;
+  state: StateType;
+  effects: {
+    getLists: Effect;
+    getPage: Effect;
+    cleanType: Effect; // 清除分类信息
+    setType: Effect;
+  };
+  reducers: {
+    changeMenuList: Reducer<StateType>;
+    changeMenuPage: Reducer<StateType>;
+    cleanTypeHandle: Reducer<StateType>;
+    setTypeHandle: Reducer<StateType>
+  };
+}
+
+const Model: LoginModelType = {
+  namespace: 'menu',
+
+  state: {
+    menuList: [],
+    page: 1,
+    pageSize: 10,
+    total: 0,
+    typeList: [],
+    type_id: null
+  },
+
+  effects: {
+    *getLists({ payload }, { call, put, select }) {
+      const page = yield select(state =>state.menu.page)
+      const pageSize = yield select(state =>state.menu.pageSize)
+      const blog_type_id = yield select(state => state.menu.type_id)
+      const response = yield call(getList, {page, per_page: pageSize, blog_type_id });
+      yield put({
+        type: 'changeMenuList',
+        payload: response,
+      });
+    },
+
+    *getPage({payload}, { call, put }) {
+      yield put({
+        type: 'changeMenuPage',
+        payload: payload.page
+      });
+    },
+
+    // 清空分类信息
+    *cleanType({ payload }, { call, put }) {
+      console.log(111)
+      yield put ({
+        type: 'cleanTypeHandle',
+      })
+    },
+
+    // 设置分类信息
+    *setType({ payload }, { call, put }) {
+      console.log(payload)
+      yield put ({
+        type: 'setTypeHandle',
+        payload
+      })
+    }
+
+  },
+
+  reducers: {
+    changeMenuList(state, { payload }) {
+      return {
+        ...state,
+        menuList: payload.data.list,
+        total: payload.data.count
+      };
+    },
+    changeMenuPage(state, { payload }) {
+      return {
+        ...state,
+        page: payload,
+      }
+    },
+    cleanTypeHandle (state) {
+      return {
+        ...state,
+        page: 1,
+        total: 0,
+        type_id: null
+      }
+    },
+    setTypeHandle (state, { payload }) {
+      return {
+        ...state,
+        page: 1,
+        total: 0,
+        type_id: payload.id
+      }
+    }
+  },
+};
+
+export default Model;
