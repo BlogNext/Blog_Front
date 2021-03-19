18382ac921f95df1b2b34b62708f48c769352789
LaughingZhu <474268433@qq.com>
2021-03-19 17:58:37 +0800

    分包

--- config/config.js
+++ config/config.js
@@ -65,7 +65,7 @@ const config = {
           webpackChunkName: true,
         },
         title: `LaughingZhu's Blog`,
-        dll: true,
+        // dll: true,
         routes: {
           exclude: [
             /models\//,
@@ -120,6 +120,70 @@ const config = {
   history: 'hash',
   runtimePublicPath: true,
   hash: true,
+  chainWebpack: function(config, { webpack }) {
+    config.merge({
+        optimization: {
+            minimize: true,
+            splitChunks: {
+                chunks: 'async', 
+                minSize: 20000, //文件最小打包体积，单位byte，默认30000，若单个文件不满足会合并其他文件组成一个
+                minChunks: 2, //最小使用到次数，超过2次执行
+                automaticNameDelimiter: '.', //连接符
+                cacheGroups: {
+                    vendors: {
+                        // 基本框架
+                        name: 'vendors',
+                        test: /^.*node_modules[\\/](?!react|react-dom).*$/,
+                        chunks: 'all',
+                        priority: 10,
+                    },
+                    'antd': {
+                      // 异步加载echarts包
+                      name: 'antd',
+                      test: /^.*node_modules[\\/](?!antd).*$/,
+                      chunks: 'all',
+                      priority: 10, // 高于async-commons优先级
+                    },
+                    'quill': {
+                      // 异步加载echarts包
+                      name: 'quill',
+                      test: /^.*node_modules[\\/](?!quill|quill-emoji).*$/,
+                      chunks: 'all',
+                      priority: 10, // 高于async-commons优先级
+                    },
+                    'highlight': {
+                      // 异步加载echarts包
+                      name: 'highlight',
+                      test: /^.*node_modules[\\/](?!highlight).*$/,
+                      chunks: 'all',
+                      priority: 10, // 高于async-commons优先级
+                    },
+                    'async-commons': {
+                        // 其余异步加载包
+                        chunks: 'async',
+                        minChunks: 2,
+                        name: 'async-commons',
+                        priority: 9,
+                    },
+                    commons: {
+                        // 其余同步加载包
+                        chunks: 'all',
+                        minChunks: 2,
+                        name: 'commons',
+                        priority: 8,
+                    },
+                },
+            },
+        },
+    });
+    //过滤掉momnet的那些不使用的国际化文件
+    config
+        .plugin('replace')
+        .use(require('webpack').ContextReplacementPlugin)
+        .tap(() => {
+            return [/moment[/\\]locale$/, /zh-cn/];
+    });
+  },
   // ssr: true,
 };
 export default config;
