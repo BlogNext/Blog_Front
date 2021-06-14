2bca7daafa3ee074d43ec1876eed101eae5c8dc8
LaughingZhu <474268433@qq.com>
2021-06-14 13:56:18 +0800

    开启hash

--- app/web/config/config.ts
+++ app/web/config/config.ts
@@ -3,7 +3,7 @@
  * @Author: LaughingZhu
  * @Date: 2021-05-28 16:38:09
  * @LastEditros: 
- * @LastEditTime: 2021-06-13 12:53:09
+ * @LastEditTime: 2021-06-14 13:51:28
  */
 import { defineConfig } from 'umi';
 import { join } from 'path';
@@ -17,7 +17,7 @@ export default defineConfig({
     // forceInitial: true,
     // mode: 'stream'
   },
-  hash: false,
+  hash: true,
   outputPath: '../public',
   manifest: {
     fileName: '../../config/manifest.json',
